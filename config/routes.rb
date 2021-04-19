841ef4f61b77797bd7a0a35b542ec4a247c3277a
Haruki Kiyama <aladdin.magi101@gmail.com>
2021-04-19 02:46:36 +0000

    [Fix] about link, side bar link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about'
+  get 'home/about' => 'homes#about'
   resources :books, only: [:create, :edit, :update, :index, :show, :destroy]
   resources :users, only: [:index, :edit, :update, :show]
 end
